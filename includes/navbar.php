30102e52f01f9f84e661281f6d0e6a58203004d5
Henoc N'GASAMA <ngasamah@gmail.com>
2025-08-30 13:08:29 +0000

    Add of navbar and footer design

---
+++ includes/navbar.php
@@ -0,0 +1,53 @@
+
+<nav class="navbar navbar-expand-lg navbar-custom">
+    <div class="container-fluid">
+
+        <!-- Brand -->
+        <a class="navbar-brand fw-bold d-flex align-items-center gap-2 magenta-color" href="#">
+            <h5 class="fw-bold primary-color">
+                <i class='bx bx-store'></i> MyShop
+            </h5>
+        </a>
+
+
+        <!-- Toggler -->
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <!-- Menu centré -->
+        <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+            <ul class="navbar-nav gap-2 mx-4">
+                <li class="nav-item">
+                    <a class="nav-link d-flex align-items-center <?php echo (basename($_SERVER['PHP_SELF']) == 'index.php') ? 'active' : ''; ?>" href="index.php">
+                        <i class='bx bx-home'></i> Home
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link <?php echo (strpos($_SERVER['PHP_SELF'], 'products') !== false) ? 'active' : ''; ?>" href="products/">
+                        <i class='bx bx-box'></i> Products
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link <?php echo (strpos($_SERVER['PHP_SELF'], 'orders') !== false) ? 'active' : ''; ?>" href="orders/">
+                        <i class='bx bx-cart'></i> Orders
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link <?php echo (strpos($_SERVER['PHP_SELF'], 'users') !== false) ? 'active' : ''; ?>" href="users/">
+                        <i class='bx bx-user'></i> Users
+                    </a>
+                </li>
+            </ul>
+        </div>
+
+        <!-- Actions -->
+        <div class="d-flex align-items-center">
+            <a href="users/login.php" class="btn btn-login">Log In</a>
+            <a href="users/register.php" class="btn btn-signup">Sign Up</a>
+        </div>
+    </div>
+</nav>
+
+<!-- Bootstrap JS -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
