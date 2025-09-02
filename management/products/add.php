9ca514b02bbb951b5f3bcaf064466671cdd0c38d
Henoc N'GASAMA <ngasamah@gmail.com>
2025-09-02 00:22:30 +0000

    Add of post credentials

--- management/products/add.php
+++ management/products/add.php
@@ -18,7 +18,7 @@
     <main class="container my-4">
         <h2 class="mb-4 text-center">Ajouter un nouveau produit</h2>
 
-        <form id="productForm" enctype="multipart/form-data" class="form-container">
+        <form id="productForm" enctype="multipart/form-data" class="form-container" method="POST" action="save.php">
             <!-- Champs cachés pour les images -->
             <input type="file" id="mainImageInput" name="mainImage" style="display: none;" accept="image/*">
             <input type="file" id="carouselImagesInput" name="carouselImages[]" style="display: none;" accept="image/*" multiple>
